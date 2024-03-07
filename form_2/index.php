d5d41d7720f0d9d2ffd93226caf38777bb240499
URLbug <gemslove2@gmail.com>
2024-03-07 19:12:10 +0200

    test 2

--- form_2/index.php
+++ form_2/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<head>
     <title>Форма для чтения CSV файлов</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
