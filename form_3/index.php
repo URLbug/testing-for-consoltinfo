f92788d96a0af259b16988c6a75fd900cf3242ee
URLbug <gemslove2@gmail.com>
2024-03-07 19:03:09 +0200

    test 3

---
+++ form_3/index.php
@@ -0,0 +1,22 @@
+<?php
+
+function natural_number(int $number, array $multiples): int
+{
+    $sum = 0;
+    
+    foreach ($multiples as $mul) {
+        for ($num = 0; $num < $number; $num++)
+            $sum += ($num % $mul) === 0 ? $num : 0;
+    }
+
+    return $sum;
+}
+
+$multiples = [
+    3,
+    5,
+];
+
+echo "Ответ - " . natural_number(1000, $multiples);
+
+?>
